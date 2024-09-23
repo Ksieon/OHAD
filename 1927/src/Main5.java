e32a5463107b97de55d3f6c779b049144712ec36
sieon <keumsiun0503@gmail.com>
2024-09-23 20:26:28 +0900

    1927

---
+++ 1927/src/Main5.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if(num==0){
+                if(pq.isEmpty()){
+                    sb.append(0).append("\n");
+                }
+                else{
+                    sb.append(pq.poll()).append("\n");
+                }
+            }
+            else{
+                pq.add(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
