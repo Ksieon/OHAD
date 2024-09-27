f4f59e166cc23f5d1a53aff14734759f376f7db6
sieon <keumsiun0503@gmail.com>
2024-09-27 18:02:52 +0900

    14235

---
+++ 14235/src/Main8.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+            st = new StringTokenizer(str, " ");
+            int num = Integer.parseInt(st.nextToken());
+            if(num==0){
+                if(pq.isEmpty()){{
+                    sb.append(-1).append("\n");
+                }}
+                else{
+                    sb.append(pq.poll()).append("\n");
+                }
+            }
+            else{
+                for(int j=0; j<num; j++){
+                    pq.add(Integer.parseInt(st.nextToken()));
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
