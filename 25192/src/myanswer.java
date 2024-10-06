d6aa551fd96c0c82f9e2c1cab947a4270fd61931
sieon <keumsiun0503@gmail.com>
2024-10-06 13:49:45 +0900

    25192

---
+++ 25192/src/myanswer.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main14 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str;
+        boolean enterCheck = false;
+        int count = 0;
+        HashMap<String, Integer> map = new HashMap<>();
+        while(N-->0){
+            str = br.readLine();
+            if(str.equals("ENTER")){
+                map = new HashMap<>();
+                enterCheck = true;
+            }
+            else if(enterCheck==true){
+                boolean nameCheck = map.containsKey(str);
+                if(!nameCheck){
+                    map.put(str, 0);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
