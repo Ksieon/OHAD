6fa5367fc4158939e56510aff17961be60ee4ac2
sieon <keumsiun0503@gmail.com>
2024-10-06 14:41:37 +0900

    4358

---
+++ 4358/src/myanswer.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main15 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<String, Integer> map = new HashMap<>();
+        int countTree = 0;
+        while(true){
+            String str = br.readLine();
+            if(str.equals("")){
+                break;
+            }
+            if(!map.containsKey(str)){
+                map.put(str, 1);
+            }
+            else{
+                int countBell = map.get(str);
+                map.put(str, ++countBell);
+            }
+            countTree++;
+        }
+        Set<String> keys = map.keySet();
+        ArrayList<String> keylist = new ArrayList<>(keys);
+        Collections.sort(keylist);
+        Iterator<String> it = keylist.iterator();
+        StringBuilder sb = new StringBuilder();
+        while(it.hasNext()){
+            String s1 = it.next();
+            int numOfBell = map.get(s1);
+            String percent = String.format("%.4f", ((float)(numOfBell)/countTree)*100);
+            sb.append(s1).append(" ").append(percent).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
