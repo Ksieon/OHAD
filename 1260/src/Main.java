3ddd812e7ff07fa6439c6100c01fdbb34e98521c
sieon <keumsiun0503@gmail.com>
2024-11-07 17:19:55 +0900

    1260

--- 1260/src/Main.java
+++ 1260/src/Main.java
@@ -7,18 +7,27 @@ class Main{
     static int N;
     static int M;
     static int V;
-    static int count;
     static Queue<Integer> queue = new LinkedList<>();
+    static Stack<Integer> stack = new Stack<>();
+
     public static void DFS(int start){
+        stack.push(start);
         Visited_arr[start] = true;
         System.out.print(start + " ");
-        if(count==N){
-            return;
-        }
-        count++;
-        for(int i=0; i<=N; i++){
-            if(Edge_arr[start][i]==1 && Visited_arr[i] == false){
-                DFS(i);
+        while(!stack.isEmpty()){
+            int vertex = stack.peek();
+            boolean hasUnvisited = false;
+            for(int i = 1; i <= N; i++){
+                if(Edge_arr[vertex][i] == 1 && !Visited_arr[i]){
+                    stack.push(i);
+                    Visited_arr[i] = true;
+                    System.out.print(i + " ");
+                    hasUnvisited = true;
+                    break;
+                }
+            }
+            if(!hasUnvisited) {
+                stack.pop();
             }
         }
     }
@@ -29,9 +38,7 @@ class Main{
 
 		while( !queue.isEmpty() ) {
 			start = queue.poll();
-
 			for(int i=1; i<=N; i++) {
-
 				if(Edge_arr[start][i] == 1 && Visited_arr[i] == false) {
 					queue.offer(i);
 					Visited_arr[i] = true;
@@ -39,9 +46,6 @@ class Main{
 				}
 			}
 		}	
-    }
-    public static void BFS2(int start){
-        
     }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -62,7 +66,6 @@ class Main{
             Edge_arr[x][y] = Edge_arr[y][x] = 1;
         }
         Visited_arr = new boolean[1001];
-        count = 0;
         DFS(V);
         System.out.println();
         Visited_arr = new boolean[1001];
