41125bb8351e27a9ca7e78b902ffbe4e029f5a21
Ksieon <keumsiun0503@gmail.com>
2025-02-27 00:00:45 +0900

    10986 not solved

---
+++ 10986/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static long M, count = 0;
+    static long[] A;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Long.parseLong(st.nextToken());
+        A = new long[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=1; i<=N; i++){
+            A[i] = A[i-1] + Long.parseLong(st.nextToken());
+        }
+
+        for(int i=1; i<=N; i++){
+            for(int j=i; j<=N; j++){
+                if((A[j]-A[i-1])%M==0){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
