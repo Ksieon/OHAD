e0a3858f10878afcc2738e6271f55a60ce1eff16
Ksieon <keumsiun0503@gmail.com>
2025-02-28 16:02:27 +0900

    250228 update explanation

--- 11659/src/Main.java
+++ 11659/src/Main.java
@@ -6,12 +6,12 @@ public class Main {
     static int[] S;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //1. N, M 입력 받기
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-
+        //2. 누적 합을 저장할 S 선언 및 값 할당
         S = new int[N+1];
         st = new StringTokenizer(br.readLine());
         for(int i=1; i<=N; i++){
@@ -19,11 +19,13 @@ public class Main {
         }
 
         StringBuilder sb = new StringBuilder();
+        //3. M번 반복하며 구간 i, j 받기
         while(M-->0){
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            sb.append(S[end]-S[start-1]).append("\n");
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            //4. i~j까지의 합을 구하기 위해 누적합 방식 활용
+            sb.append(S[j]-S[i-1]).append("\n");
         }
         System.out.print(sb.toString());
     }
--- 2096/src/Main.java
+++ 2096/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
             dp[i][1] = Integer.min(dp[i-1][1] + nums[i][1], Integer.min(dp[i-1][0] + nums[i][1], dp[i-1][2] + nums[i][1]));
             dp[i][2] = Integer.min(dp[i-1][2] + nums[i][2], dp[i-1][1] + nums[i][2]);
         }
+        // 숫자의 최대값은 9이고, N<=100,000이므로 최대 값 900,000
         int min = 900001;
         for(int numOflastline : dp[N]){
             min = Integer.min(min, numOflastline);
@@ -29,17 +30,18 @@ public class Main {
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //1. 숫자표 행의 개수 N
         N = Integer.parseInt(br.readLine());
         nums = new int[N+1][3];
         StringTokenizer st;
+        //2. nums 배열의 0, 1, 2번 위치에 숫자 입력 받기 (숫자표)
         for(int i=1; i<=N; i++){
             st = new StringTokenizer(br.readLine());
             nums[i][0] = Integer.parseInt(st.nextToken());
             nums[i][1] = Integer.parseInt(st.nextToken());
             nums[i][2] = Integer.parseInt(st.nextToken());
         }
-
+        //3. dp배열에 현재 위치에서 최대(최소) 값을 저장하며 최종적인 해 도출
         dp = new int[N+1][3];
         System.out.println(calculateMaxScore() + " " + calculateMinScore());
     }
